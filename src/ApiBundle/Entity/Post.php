92345f7470786697d6ca349c4fc7027baf890286
kazimirchuk <lekskazimirchuk@gmail.com>
2016-06-01 21:11:43 +0300

    Added .idea to .gitignore, added title to post.

--- src/ApiBundle/Entity/Post.php
+++ src/ApiBundle/Entity/Post.php
@@ -8,55 +8,55 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\Table(name="posts")
  */
- class Post implements \JsonSerializable
- {
-   /**
-    * @ORM\Column(type="integer")
-    * @ORM\Id
-    * @ORM\GeneratedValue(strategy="AUTO")
-    */
-   protected $id;
+class Post implements \JsonSerializable
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
-   /**
-    * @ORM\Column(type="string", length=100)
-    */
-   protected $title;
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $title;
 
-   /**
-    * @return int
-    */
-   public function getId() {
-       return $this->id;
-   }
+    /**
+     * @return int
+     */
+    public function getId() {
+        return $this->id;
+    }
 
-   /**
+    /**
     * @return string
     */
-   public function getTitle() {
-       return $this->title;
-   }
+    public function getTitle() {
+        return $this->title;
+    }
 
-   /**
+    /**
     * @param int $id
     */
-   public function setId($id)
-   {
-       $this->id = $id;
-   }
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
 
-   /**
+    /**
     * @param string $title
     */
-   public function setTitle($title)
-   {
-       $this->title = $title;
-   }
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
 
-   public function jsonSerialize()
+    public function jsonSerialize()
     {
         return [
             'id' => $this->id,
             'title'=> $this->title,
         ];
     }
- }
+}
