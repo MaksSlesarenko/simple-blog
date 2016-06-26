4338f8b64e4558006f0d0f74f8d2e527543503d9
reillyethan <lekskazimirchuk@gmail.com>
2016-06-26 17:54:16 +0300

    Removed unnecessary bundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
-            new Liip\ImagineBundle\LiipImagineBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
             new ApiBundle\ApiBundle()
