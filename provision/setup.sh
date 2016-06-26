c45ee843dd9da5fa5c7502e49d0c6b09a2b82775
reillyethan <lekskazimirchuk@gmail.com>
2016-06-26 17:54:01 +0300

    Added admin user in setup.sh

--- provision/setup.sh
+++ provision/setup.sh
@@ -63,4 +63,7 @@ sed -i "s@database_user:.*@database_user: $DB_USER@" $ROOT_DIR/app/config/parame
 
 bin/console doctrine:schema:update --force
 bin/console doctrine:fixtures:load
+bin/console fos:user:create admin dj@khaled.com thekeytosuccess
+bin/console fos:user:activate admin
+bin/console fos:user:promote admin ROLE_ADMIN
 bin/console assets:install
